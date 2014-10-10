eda9a9e127bdf4fe7459aadd29c2c3b7c1bd9705
tomomura <am.n04a@gmail.com>
2014-10-11 02:31:27 +0900

    * トップページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,12 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback',  to: 'users/sessions#callback'
   post '/auth/:provider/callback', to: 'users/sessions#callback'
 
+  resources :users, only: :none do
+    collection do
+      get :sign_in
+    end
+  end
+
+  root "users#sign_in"
+
 end
