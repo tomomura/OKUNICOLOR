851f855c6be3caed5db6ab49ccc175443894824f
tomomura <am.n04a@gmail.com>
2014-10-11 02:52:21 +0900

    * 認証失敗時の処理を実装

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -7,7 +7,15 @@ class Users::SessionsController < ApplicationController
     auth = request.env['omniauth.auth']
     user = User.find_by(provider: auth['provider'], uid: auth['uid']) || User.create_with_twitter_omniauth(auth)
     session[:user_id] = user.id
+
     redirect_to root_path
+  end
+
+  #
+  #=== 認証失敗時に呼び出される
+  #
+  def failure
+    redirect_to root_path, alert: params[:message]
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback',  to: 'users/sessions#callback'
   post '/auth/:provider/callback', to: 'users/sessions#callback'
 
+  get '/auth/failure',  to: 'users/sessions#failure'
+
   resources :users, only: :none do
     collection do
       get :sign_in
