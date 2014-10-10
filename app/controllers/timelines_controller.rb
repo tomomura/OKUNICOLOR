0f20bde1a86e0174a3fe51667ad623a391542632
yukimeshi <yukimeshi@yukimeshi.com>
2014-10-11 04:01:50 +0900

    指摘事項を修正

--- app/controllers/timelines_controller.rb
+++ app/controllers/timelines_controller.rb
@@ -1,5 +1,5 @@
 class TimelinesController < ApplicationController
   def index
-    render json:Color.request(*params[:keyword].to_s.split(','))
+    render json: Color.request(*params[:keyword].to_s.split(','))
   end
 end
